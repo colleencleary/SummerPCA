998674d0a929f57dd38d4d1612f018395361a649
Colleen Cleary <cleary.ce@gmail.com>
2016-06-22 09:54:38 -0400

    Updated with color function

--- PCA_color.py
+++ PCA_color.py
@@ -1,15 +1,15 @@
 
-def colorColumns(newfiPh):
+def colorColumns(newfiPh, BDdict):
     '''This function returns an array containing color values'''
+  
     colnames=newfiPh.columns.tolist()
     #colnames=colnames[6:]
     
     for band in colnames[6:] :
-        for i in colnames[6:] :
-            if i not in band :
-                newcol = band + '-' + i
-                newfiPh[newcol]=newfiPh[band]-newfiPh.i
-            else :
-                pass
+        for n in colnames[6:] :
+            if BDdict[band] < BDdict[n] :
+                    newcol = band + '-' + n
+                    newfiPh[newcol]=newfiPh[band]-newfiPh[n]               
+            
 
     return newfiPh
--- PCA_plot.py
+++ PCA_plot.py
@@ -2,25 +2,32 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.decomposition import PCA
 
-def plot_PCA(data_PCA, y_value, z_value, data_scaled, factor, pca):
-    plt.scatter(data_PCA, y_value, c=z_value)
+def plot_PCA(x_value, y_value, z_value, data_scaled, factor, pca, loadings, save):
+    plt.scatter(x_value, y_value, c=z_value)
     #plt.ylim([-4,4])
-    plt.xlim([-2,-8])
-    plt.title('PCA of Star Data')
-    plt.colorbar().set_label('Gravity')
-    plt.xlabel('PCA[0]')
-    plt.ylabel('PCA[3]')
+    #plt.xlim([-4,4])
+    #plt.title('PCA of Star Data')
     
-    loadings=pd.DataFrame(pca.components_,columns=data_scaled.columns.tolist())
-    loadings_sq=loadings**2
-    loadings_sq=loadings_sq.transpose()
-
-    plt.arrow(0,0,loadings.Temperature[0]*factor, loadings.Temperature[3]*factor, color='red')
-    plt.arrow(0,0,loadings.Luminosity[0]*factor, loadings.Luminosity[3]*factor, color='blue')
-    plt.arrow(0,0, loadings.Gravity[0]*factor, loadings.Gravity[3]*factor, color='green')
-    plt.text(loadings.Temperature[0]*factor, loadings.Temperature[3]*factor, 'Temperature', color='red')
-    plt.text(loadings.Luminosity[0]*factor, loadings.Luminosity[3]*factor, 'Luminosity', color='blue')
-    plt.text(loadings.Gravity[0]*factor, loadings.Gravity[3]*factor, 'Gravity', color='green')
-
+     
+    plt.colorbar().set_label(z_value.name)
+    plt.xlabel(x_value.name)
+    plt.ylabel(y_value.name)
+    plt.gca().invert_yaxis()
+    
+    if loadings==1:
+        loadings=pd.DataFrame(pca.components_,columns=data_scaled.columns.tolist())
+        loadings_sq=loadings**2
+        loadings_sq=loadings_sq.transpose()
+    
+        plt.arrow(0,0,loadings.Temperature[x_value.name]*factor, loadings.Temperature[y_value.name]*factor, color='red')
+        plt.arrow(0,0,loadings.Luminosity[x_value.name]*factor, loadings.Luminosity[y_value.name]*factor, color='blue')
+        plt.arrow(0,0, loadings.Gravity[x_value.name]*factor, loadings.Gravity[y_value.name]*factor, color='green')
+        plt.text(loadings.Temperature[x_value.name]*factor, loadings.Temperature[y_value.name]*factor, 'Temperature', color='red')
+        plt.text(loadings.Luminosity[x_value.name]*factor, loadings.Luminosity[y_value.name]*factor, 'Luminosity', color='blue')
+        plt.text(loadings.Gravity[x_value.name]*factor, loadings.Gravity[y_value.name]*factor, 'Gravity', color='green')
+    else: pass
+    if save==1:
+        plt.savefig('/Users/Owner/Summer_Research/graphs/'+x_value.name+'_'+y_value.name+'_'+z_value.name+'.png') 
+    else: pass
     plt.show()
 
